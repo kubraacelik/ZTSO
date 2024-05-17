977b3c56ea3c48a12964fd9bbec5da2487637ef4
kubraacelik <kkbra.celik92@gmail.com>
2024-05-17 17:26:45 +0300

    updated slider

--- ztso/src/components/Slider.jsx
+++ ztso/src/components/Slider.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import "../styles/Slider.css";
-import { useState } from "react";
+import Slider1 from "../assets/slider1.png";
+import Slider2 from "../assets/slider2.jpg";
+import Slider3 from "../assets/slider3.jpg";
+import Slider4 from "../assets/slider4.jpg";
+import Slider5 from "../assets/slider5.jpg";
 
 function Slider() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -12,66 +17,33 @@ function Slider() {
     setCurrentSlide((prevSlide) => (prevSlide === 0 ? 4 : prevSlide - 1));
   };
 
+  const slides = [Slider1, Slider2, Slider3, Slider4, Slider5];
+
   return (
     <>
       <div className="slider">
-        <div className={currentSlide === 0 ? "slide active" : "slide"}></div>
-        <div className="inner">
-          <h1>Slide Başlığı 1</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi
-            repellendus eveniet, maiores voluptatem exercitationem distinctio
-            excepturi velit! Quaerat, alias quasi?
-          </p>
-        </div>
-        <div className={currentSlide === 1 ? "slide active" : "slide"}>
-          <div className="inner">
-            <h1>Slide Başlığı 2</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi
-              repellendus eveniet, maiores voluptatem exercitationem distinctio
-              excepturi velit! Quaerat, alias quasi?
-            </p>
+        <div className="list">
+          {slides.map((slide, index) => (
+            <div key={index} className={index === currentSlide ? "item active" : "item"}>
+             <div className='slide-img'>
+             <img src={slide} alt="" />
+             </div>
+              <div className="content">
+                <p>Design</p>
+                <h2>Slider {index + 1}</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+                  sapiente, assumenda autem magnam ipsam laboriosam!
+                </p>
+              </div>
+            </div>
+          ))}
+          {/* buttons */}
+          <div className="arrows">
+            <button id="prev" onClick={prevSlide}>&lt;</button>
+            <button id="next" onClick={nextSlide}>&gt;</button>
           </div>
         </div>
-        <div className={currentSlide === 2 ? "slide active" : "slide"}>
-          <div className="inner">
-            <h1>Slide Başlığı 3</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi
-              repellendus eveniet, maiores voluptatem exercitationem distinctio
-              excepturi velit! Quaerat, alias quasi?
-            </p>
-          </div>
-        </div>
-        <div className={currentSlide === 3 ? "slide active" : "slide"}>
-          <div className="inner">
-            <h1>Slide Başlığı 4</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi
-              repellendus eveniet, maiores voluptatem exercitationem distinctio
-              excepturi velit! Quaerat, alias quasi?
-            </p>
-          </div>
-        </div>
-        <div className={currentSlide === 4 ? "slide active" : "slide"}>
-          <div className="inner">
-            <h1>Slide Başlığı 5</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi
-              repellendus eveniet, maiores voluptatem exercitationem distinctio
-              excepturi velit! Quaerat, alias quasi?
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="indicators">
-        <button id="prev" onClick={prevSlide}>
-          <i className="fas fa-angle-left"></i>
-        </button>
-        <button id="next" onClick={nextSlide}>
-          <i className="fas fa-angle-right"></i>
-        </button>
       </div>
     </>
   );
