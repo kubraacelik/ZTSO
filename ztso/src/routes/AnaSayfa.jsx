454809137db30d08b206fc1603d0799e1250b76c
kubraacelik <kkbra.celik92@gmail.com>
2024-05-17 19:14:07 +0300

    added SlideMenu

--- ztso/src/routes/AnaSayfa.jsx
+++ ztso/src/routes/AnaSayfa.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
+import SlideMenu from "../components/SlideMenu";
 
 export const AnaSayfa = () => {
   return (
     <div>
       <Navbar />
       <Slider />
+      <SlideMenu />
     </div>
   );
 };
