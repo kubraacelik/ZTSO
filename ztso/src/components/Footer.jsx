937b6c0449796115900b8d2a6128370dcf446c16
kubraacelik <kkbra.celik92@gmail.com>
2024-05-26 16:57:29 +0300

    updated components

--- ztso/src/components/Footer.jsx
+++ ztso/src/components/Footer.jsx
@@ -14,111 +14,107 @@ function Footer() {
           <div className="footer-body-menu">
             <ul>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/hakkimizda">
+                <a href="https://www.ztso.org.tr/icerik/hakkimizda" target="_blank">
                   Hakkımızda
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/vizyon-misyon">
+                <a href="https://www.ztso.org.tr/icerik/vizyon-misyon" target="_blank">
                   Vizyon-Misyon
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/baskandan-mesaj">
+                <a href="https://www.ztso.org.tr/icerik/baskandan-mesaj" target="_blank">
                   Başkandan Mesaj
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/kurumsal-kimlik">
+                <a href="https://www.ztso.org.tr/icerik/kurumsal-kimlik" target="_blank">
                   Kurumsal Kimlik
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/profil-kategori/yonetim-kurulu">
+                <a href="https://www.ztso.org.tr/profil-kategori/yonetim-kurulu" target="_blank">
                   Yönetim Kurulu
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/profil-kategori/meclis-uyeleri">
+                <a href="https://www.ztso.org.tr/profil-kategori/meclis-uyeleri" target="_blank">
                   Meclis Üyeleri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/komisyon-ve-kurullar">
+                <a href="https://www.ztso.org.tr/icerik/komisyon-ve-kurullar" target="_blank">
                   Komisyon ve Kurallar
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/organizasyon-semasi">
+                <a href="https://www.ztso.org.tr/icerik/organizasyon-semasi" target="_blank">
                   Organizasyon Şeması
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/profil-kategori/zonguldak-tso-personeli">
+                <a href="https://www.ztso.org.tr/profil-kategori/zonguldak-tso-personeli" target="_blank">
                   Odamız Personeli
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/mevzuat">Mevzuat</a>
+                <a href="https://www.ztso.org.tr/icerik/mevzuat" target="_blank">Mevzuat</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/politikalarimiz">
-                  Politikalarımız
-                </a>
+                <a href="https://www.ztso.org.tr/icerik/politikalarimiz" target="_blank">Politikalarımız</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://ztso.org.tr/dijitalebulten/100.yil/">
-                  100. Yıl Kitabı
-                </a>
+                <a href="https://ztso.org.tr/dijitalebulten/100.yil/" target="_blank">100. Yıl Kitabı</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/il-genc-girisimci-kurulu">
+                <a href="https://www.ztso.org.tr/icerik/il-genc-girisimci-kurulu" target="_blank">
                   İl Genç Girişimci Kurulu
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/il-kadin-girisimci-kurulu">
+                <a href="https://www.ztso.org.tr/icerik/il-kadin-girisimci-kurulu" target="_blank">
                   İl Kadın Girişimci Kurulu
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/404">Akretide Oda</a>
+                <a href="https://www.ztso.org.tr/404" target="_blank">Akretide Oda</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/uyelik-ve-istirakler">
+                <a href="https://www.ztso.org.tr/icerik/uyelik-ve-istirakler" target="_blank">
                   Üyelik ve İştirakler
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/404">
+                <a href="https://www.ztso.org.tr/404" target="_blank">
                   Eski Yönetim Kurulu Başkanlarımız
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/404">ZTSO Etik Kuralları</a>
+                <a href="https://www.ztso.org.tr/404" target="_blank">ZTSO Etik Kuralları</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/faaliyet-raporlari">
+                <a href="https://www.ztso.org.tr/icerik/faaliyet-raporlari" target="_blank">
                   Faaliyet Raporları
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/404">
+                <a href="https://www.ztso.org.tr/404" target="_blank">
                   ZTSO Dış Ticaret Destek Ofisi
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://ztso.org.tr/uploads/files/Fizibiliteler/isplanisonhali.xlsx">
+                <a href="https://ztso.org.tr/uploads/files/Fizibiliteler/isplanisonhali.xlsx" target="_blank">
                   ZTSO İş Planı
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/acil-durum-eylem-plani">
+                <a href="https://www.ztso.org.tr/icerik/acil-durum-eylem-plani" target="_blank">
                   Acil Durum Eylem Planı
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://ztso.org.tr/dijitalebulten/kaliteelkitabi/">
+                <a href="https://ztso.org.tr/dijitalebulten/kaliteelkitabi/" target="_blank">
                   Kalite El Kitabı
                 </a>
               </li>
@@ -127,80 +123,78 @@ function Footer() {
           <div className="footer-body-menu">
             <ul>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/ticaret-sicil-islemleri">
+                <a href="https://www.ztso.org.tr/icerik/ticaret-sicil-islemleri" target="_blank">
                   Ticaret Sicil İşlemleri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/oda-sicil-islemleri">
+                <a href="https://www.ztso.org.tr/icerik/oda-sicil-islemleri" target="_blank">
                   Oda Sicil İşlemleri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/kapasite-raporlari">
+                <a href="https://www.ztso.org.tr/icerik/kapasite-raporlari" target="_blank">
                   Kapasite Raporları
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/k-belgeleri">
-                  K Belgeleri
-                </a>
+                <a href="https://www.ztso.org.tr/icerik/k-belgeleri" target="_blank">K Belgeleri</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/sigortacilik-levha-kaydi">
+                <a href="https://www.ztso.org.tr/icerik/sigortacilik-levha-kaydi" target="_blank">
                   Sigortacılık Levha Kaydı
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/faaliyet-belgesi">
+                <a href="https://www.ztso.org.tr/icerik/faaliyet-belgesi" target="_blank">
                   Faaliyet Belgesi
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/404">Mersis</a>
+                <a href="https://www.ztso.org.tr/404" target="_blank">Mersis</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/yerli-mali-belgesi">
+                <a href="https://www.ztso.org.tr/icerik/yerli-mali-belgesi" target="_blank">
                   Yerli Malı Belgesi
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/fiili-sarfiyat-belgesi">
+                <a href="https://www.ztso.org.tr/icerik/fiili-sarfiyat-belgesi" target="_blank">
                   Filli Sarfiyat Belgesi
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/dolasim-belgeleri-mense-atr-eur1">
+                <a href="https://www.ztso.org.tr/icerik/dolasim-belgeleri-mense-atr-eur1" target="_blank">
                   Dolaşım Belgeleri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/is-makinelerinin-tescili">
+                <a href="https://www.ztso.org.tr/icerik/is-makinelerinin-tescili" target="_blank">
                   İş Makineleri Tescili
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/onay-hizmetleri">
+                <a href="https://www.ztso.org.tr/icerik/onay-hizmetleri" target="_blank">
                   Onay Hizmetleri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/ciraklik-sozlesmesi-onayi">
+                <a href="https://www.ztso.org.tr/icerik/ciraklik-sozlesmesi-onayi" target="_blank">
                   Çıraklık Sözleşmesi Onayı
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/kamu-kuruluslari-ile-ilgili-talep">
+                <a href="https://www.ztso.org.tr/icerik/kamu-kuruluslari-ile-ilgili-talep" target="_blank">
                   Kamu Kuruluşları İle İlgili Talep
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/sanayi-sicil-belgesi">
+                <a href="https://www.ztso.org.tr/icerik/sanayi-sicil-belgesi" target="_blank">
                   Sanayi Sicil Belgesi
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/ihale-durum-belgesi">
+                <a href="https://www.ztso.org.tr/icerik/ihale-durum-belgesi" target="_blank">
                   İhale Durum Belgesi
                 </a>
               </li>
@@ -209,83 +203,75 @@ function Footer() {
           <div className="footer-body-menu">
             <ul>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/zonguldak-sehir-rehberi">
+                <a href="https://www.ztso.org.tr/icerik/zonguldak-sehir-rehberi" target="_blank">
                   Zonguldak Şehir Rehberi
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/ekonomik-raporlar">
+                <a href="https://www.ztso.org.tr/icerik/ekonomik-raporlar" target="_blank">
                   Ekonomik Raporlar
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/dokumanlar">Dokümanlar</a>
+                <a href="https://www.ztso.org.tr/icerik/dokumanlar" target="_blank">Dokümanlar</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/ztso-duyurulari">
+                <a href="https://www.ztso.org.tr/icerik/ztso-duyurulari" target="_blank">
                   ZTSO Duyuruları
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/kobi-finansman">
+                <a href="https://www.ztso.org.tr/icerik/kobi-finansman" target="_blank">
                   KOBİ Finansman
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/dijital-hizmetler">
+                <a href="https://www.ztso.org.tr/icerik/dijital-hizmetler" target="_blank">
                   Dijital Hizmetler
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/doviz-kurlari">
-                  Döviz Kurları
-                </a>
+                <a href="https://www.ztso.org.tr/icerik/doviz-kurlari" target="_blank">Döviz Kurları</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/duyurular">Duyurular</a>
+                <a href="https://www.ztso.org.tr/icerik/duyurular" target="_blank">Duyurular</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/ekonomi-verileri">
+                <a href="https://www.ztso.org.tr/icerik/ekonomi-verileri" target="_blank">
                   Ekonomi Verileri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/etkinlikler">
-                  Etkinlikler
-                </a>
+                <a href="https://www.ztso.org.tr/icerik/etkinlikler" target="_blank">Etkinlikler</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/fuarlardan-haberler">
+                <a href="https://www.ztso.org.tr/icerik/fuarlardan-haberler" target="_blank">
                   Fuarlardan Haberler
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/indirimli-uye-hizmetleri">
+                <a href="https://www.ztso.org.tr/icerik/indirimli-uye-hizmetleri" target="_blank">
                   İndirimli Üye Hizmetleri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/ihracat-ithalat">
-                  İhracat-İthalat
-                </a>
+                <a href="https://www.ztso.org.tr/icerik/ihracat-ithalat" target="_blank">İhracat-İthalat</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/komitelerden-haberler">
+                <a href="https://www.ztso.org.tr/icerik/komitelerden-haberler" target="_blank">
                   Komitelerden Haberler
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/ziyaretler">
-                  Ziyaretler
-                </a>
+                <a href="https://www.ztso.org.tr/icerik/ziyaretler" target="_blank">Ziyaretler</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/uye-ziyaretleri">
+                <a href="https://www.ztso.org.tr/icerik/uye-ziyaretleri" target="_blank">
                   Üye Ziyaretleri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/odamiz-basininda">
+                <a href="https://www.ztso.org.tr/icerik/odamiz-basininda" target="_blank">
                   Odamız Basında
                 </a>
               </li>
@@ -294,20 +280,20 @@ function Footer() {
           <div className="footer-body-menu">
             <ul>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/basin-bultenleri">
+                <a href="https://www.ztso.org.tr/icerik/basin-bultenleri" target="_blank">
                   Basın Bültenleri
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/dilek-sikayet">
+                <a href="https://www.ztso.org.tr/icerik/dilek-sikayet" target="_blank">
                   Dilek ve Şikayetler
                 </a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/icerik/iletisim">İletişim</a>
+                <a href="https://www.ztso.org.tr/icerik/iletisim" target="_blank">İletişim</a>
               </li>
               <li className="footer-body-menu-item">
-                <a href="https://www.ztso.org.tr/404">Bize Ulaşın</a>
+                <a href="https://www.ztso.org.tr/404" target="_blank">Bize Ulaşın</a>
               </li>
             </ul>
           </div>
